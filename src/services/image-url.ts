04c5486b886981bd33ad35ac2b05da615a2dfeab
tota79 <balashyabni@gmail.com>
2023-07-20 11:06:53 +0200

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,6 @@
+import NoImage from "../assets/no-image.webp";
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return NoImage;
   const index = url.indexOf("media/") + "media/".length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
 };
